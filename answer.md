4f83a528d921c8dc665e6e43da64ab4915498105
tonchan1216 <viva.la.vida27315@gmail.com>
2017-08-09 17:05:27 +0900

    提出コミット

--- answer.md
+++ answer.md
@@ -1,8 +1,11 @@
 ## Q1. 得意なプログラミング言語 Favorite Programming Language
 あなたの得意なプログラミング言語を記載してみてください。
 Write what your favorite programming language is here.
+PHP
 
 ## Q2. コードの説明 Code Explanation
 あなたのコードがどのように動作するのか、簡潔に説明してください。
 Write a brief explanation about how your code works here.
-
+入力されたコマンドラインの引数に応じて条件分岐を行い，
+対応する文字列を出力する．
+もし入力引数が空文字であった場合，sys.argvに入力されない．よって，引数がない場合は空文字入力と判断し，''を追加する．
--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ def main(argv):
   # ---
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
+  if len(argv) == 0:
+  	argv = ['']
 
   for i, v in enumerate(argv):
-    print("argv[{0}]: {1}".format(i, v))
+  	if v == "World":
+  		print('Hello World!')
+  	elif len(v) == 0 and isinstance(v, str):
+  		print('Hello!')
+  	elif v == 'codecheck':
+  		print('Hello codecheck!')
+  	elif v == '織田信長':
+  		print('Hello 織田信長!')
+  	else:
+  		print('printing:' + v)
